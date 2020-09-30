c14d729905b93ba787782a8376db929e93b623e2
Prosoft-furqana <furqana@prosoftesolutions.com>
2020-09-30 20:51:15 +0530

    initial commit

---
+++ res.php
@@ -0,0 +1,112 @@
+<html>
+<head>
+	<title> Resume</title>
+</head>
+<style>
+	p{
+     border: 5px solid black
+	}
+	body{
+	 background-image: url("images/workplace.jpg");
+     background-size: cover;
+     background-image:color: white;
+    }
+</style>
+
+<body>
+	<center>
+		<h1>
+		 <u> My resume</u>
+		</h1>
+	</center>
+	<h3> FURQANA ANGOLKAR</h3>
+	<img src="images/tree.jpeg" width="150" height="130" align=right>
+	<h5> 1006B2,kalaigar galli,Fort Road  Belgavi-590001</h5>
+	<h5>furqanaangolkar@gmail.com</h5>
+	<h5>Contact no:7406564190</h5><hr>
+	<h2>
+		<div style="background-color:gray;color:white;padding:8px;">
+		<u>Career Objective</u>
+	</h2>
+	<h3> To qualify for a challenging position in a professional field where my qualification and ability can result in personal and organisational advancement and growth with sincerity.</h3><hr>
+	<h2>
+		<div style="background-color:gray;color:white;padding:05px;">
+		<u>Academic Qualification</u>
+	</h2>
+	<table border="5" width="1000">
+		<tr>
+			<th> Qualification</th>
+			<th> University</th>
+			<th> Board</th>
+			<th> Year of passing</th>
+			<th> Percentage or Sgpa</th>
+		</tr>
+		<tr>
+			<th> Computer science & Engineering</th>
+			<th> S.G Balekundri
+              Institude of Technology Belgavi</th>
+			<th> VTU Belagavi</th>
+			<th> 2019</th>
+			<th> 7.79</th>
+		</tr>
+		<tr>
+			<th> PUC or 12th</th>
+			<th> Bhartesh pu college </th>
+			<th>Pre-University Exam Board</th>
+			<th> 2015</th>
+			<th> 82%</th>
+		</tr>
+         <tr>
+			<th> SSLC or 10th</th>
+			<th>Al-Ameen English medium High school</t>
+			<th>Karnataka Secondary Education Examination Board</th>
+			<th> 2013</th>
+			<th> 76%</th>
+		</tr>
+	</table><hr>
+	<h2><div style="background-color:gray;color:white;padding:05px;">
+		<u>Technical skills</u></h2>
+		<ul>
+		<b>
+	    <h3>
+	    	<li>Programming languages: C, java,python, SQl</li>
+	    	<li>Web Technologies            : HTML, CSS
+	    	</li>
+	    	<li>Tools:Microsoft Visual studio</li>
+	    </h3>
+	</b>
+</ul><hr>
+<h2><div style="background-color:gray;color:white;padding:05px;">
+		<u>Academic mini projects</u></h2>
+		<ul>
+		<b>
+	    <h3><small>
+	    	<li>DBMS Mini project:Student record database managemet</li>
+	    	<li>Computer Graphics Mini project: Tower of hanoi
+
+	    	</li>
+	    </small>
+	    </h3>
+	</b>
+</ul>
+<h2><div style="background-color:gray;color:white;padding:05px;">
+		<center><u>Personal details</u></h2>
+		<ul>
+		<b>
+	    <h3><center>
+	    	<li>Name:Furqana.Angolkar</li>
+	    <li>Date of Birth:14/08/1997</li>
+	    <li>Gender                  :Female</li>
+	     <li>Father name          :Fazlulhaq.Angolkar</li>
+	     <li>Mother name        :Shahnaz.Angolkar</li>
+         </center>
+     </center>
+	    </h3>
+	</b>
+</ul>
+	    	
+</div>
+</p>
+</body>
+
+</html>
